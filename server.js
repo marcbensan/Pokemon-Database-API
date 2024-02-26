444f24c309c0e0f94a7484ef21ae6092d9c9913d
venomspood <marc.bensan7@gmail.com>
2024-02-25 22:40:28 -0500

    Added documentations

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const fs = require("fs");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
+// Static Files
 app.use(express.static("public"));
 
+// Main page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Query all data
 app.get("/api/data", (req, res) => {
   fs.readFile("data.json", "utf8", (err, fileData) => {
     try {
@@ -23,16 +26,18 @@ app.get("/api/data", (req, res) => {
   });
 });
 
+// Query a specific object
 app.get("/api/data/:id", (req, res) => {
+  // Parse the ID to int for comparison
   const pokemonId = parseInt(req.params.id);
 
   fs.readFile("data.json", "utf8", (err, fileData) => {
     try {
       const jsonData = JSON.parse(fileData);
+      // Find the matching Pokemon according to the ID
       const selectedPokemon = jsonData.find(
         (pokemon) => pokemon.id === pokemonId
       );
-
       res.send(selectedPokemon);
     } catch (error) {
       console.error(error);
